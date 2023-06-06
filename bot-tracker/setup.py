cb83db5ce09bdb067a47486d75be8e4e548d87b0
hsuanguo <hsuan.guo@gmail.com>
2023-06-06 10:34:21 +0100

    added botracker package

---
+++ bot-tracker/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+from setuptools import find_packages, setup
+
+package_name = "bot-tracker"
+
+if __name__ == "__main__":
+
+    # the dependency list
+    dependency_list = [
+        "cython",
+        "numpy",
+        "scipy",
+        "motmetrics",
+        "cython_bbox",
+        "matplotlib",
+        "fastreid==1.4.100",
+        "tensorboard==2.11.2",
+    ]
+
+    # final setup
+    setup(
+        name=package_name,
+        version='0.1.0',
+        python_requires=">=3.8",
+        packages=find_packages(exclude=['tests*']),
+        include_package_data=True,
+        description="Bot Tracker",
+        long_description="BoT-SORT: Robust Associations Multi-Pedestrian Tracking",
+        install_requires=dependency_list,
+        author="OostoLTD",
+        author_email="xuang@oosto.com",
+    )
